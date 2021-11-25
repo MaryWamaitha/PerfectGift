eaa4fd5656d0ff59a579fcb916ef864e22db911c
MaryWamaitha <mary.wamaitha@ashesi.edu.gh>
2021-11-25 18:54:09 +0000

    categories

--- admin/add_category.php
+++ admin/add_category.php
@@ -12,7 +12,7 @@ session_start();
 <html>
 
 <head>
-    <title>Add Brand</title>
+    <title>Add Category</title>
     <meta charset="UTF-8">
     <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
     <link rel="stylesheet" href="../CSS/style.css">
